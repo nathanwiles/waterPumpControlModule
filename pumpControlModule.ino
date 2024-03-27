fc593091975261119eac4d8a33696f16bde762f5
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2024-03-27 04:27:56 -0400

    Add refill tank functionality to pump control module

--- pumpControlModule.ino
+++ pumpControlModule.ino
@@ -1,5 +1,7 @@
 #include "waterPlants.h"
 #include "lcdController.h"
+#include "refillTank.h"
+#include "pumpControl.h"
 
 const int buttonPin = 7;  // the number of the pushbutton pin
 
@@ -8,6 +10,7 @@ int selectedDuration = 0;
 bool isStarted = false;
 
 void setup() {
+  setupPumpControl();
   pinMode(buttonPin, INPUT_PULLUP);  // initialize the pushbutton pin as an input with pullup resistor
 }
 
@@ -20,13 +23,15 @@ void loop() {
       isStarted = true;
     }
     selectedDuration += 10;
-    if (selectedDuration > 60) {
+    if (selectedDuration > 70) {
       selectedDuration = 0;  // 0 represents the cancel option
     }
     lastButtonPressTime = millis();
     DisplayItem item;
     if (selectedDuration == 0) {
       item = { 0, 0, "Cancel" };
+    } else if (selectedDuration == 70) {
+      item = { 0, 0, "Fill Main Tank" };
     } else {
       item = { 0, 0, "Duration: " + String(selectedDuration) };
     }
@@ -34,8 +39,11 @@ void loop() {
   }
 
   if (isStarted && millis() - lastButtonPressTime > 3000) {  // 3 seconds have passed
-    if (selectedDuration != 0) {
+    if (selectedDuration > 0 && selectedDuration < 70) {
       waterPlants(selectedDuration);
+    }
+    if (selectedDuration == 70) {
+      refillTank();
     }
     isStarted = false;  // go back to waiting for a button press
     toggleLCDPower(false);
